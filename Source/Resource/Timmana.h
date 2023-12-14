afe4a3e931f8916c470c91f4b43874a9563ec59f
Paxton Fleming <fleming.paxton@yahoo.com>
2023-12-14 15:14:04 -0800

    add font to project

---
+++ Source/Resource/Timmana.h
@@ -0,0 +1,10 @@
+/* (Auto-generated binary data file). */
+
+#pragma once
+
+namespace Timmana
+{
+    extern const char*  Timmana_Regular_ttf;
+    const int           Timmana_Regular_ttfSize = 1116104;
+
+}
